36e46a7be36c7bd6fbcf78e9872448acedd3cfb1
Robert Niimi <robertn702@gmail.com>
2016-02-07 16:50:46 -0800

    updates build script to launch repo build script

--- pre_build.sh
+++ pre_build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# echo "cd /var/www/$1"
+echo "cd $1"
 cd $1
-# pwd
+
 echo "git fetch --all"
 git fetch --all
+
 echo "git reset --hard origin/master"
 git reset --hard origin/master
+
+echo "bash $1/build/sh"
+bash $1/build.sh
--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post('/', (req, res) => {
   const buildPath = `${repoPath}/build.sh`;
 
   logWrapper(execFile('bash', ['./pre_build.sh', repoPath]), () => {
-    logWrapper(exec(`cd ${repoPath};bash ${buildPath}`));
+    // logWrapper(exec(`cd ${repoPath};bash ${buildPath}`));
     // logWrapper(execFile('bash', [buildPath]));
   });
 });
